dc409061222a218e1acafe09604e5a4451a0e3ca
Isaiah <i-mcnealy@outlook.com>
2020-04-07 16:16:27 -0400

    Rename SettingActivity and layout -> EditProfileActivity

--- app/src/main/java/com/isaiahmcnealy/savvyllc/EditProfileActivity.java
+++ app/src/main/java/com/isaiahmcnealy/savvyllc/EditProfileActivity.java
@@ -4,11 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SettingsActivity extends AppCompatActivity {
+public class EditProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.activity_edit_profile);
     }
 }
